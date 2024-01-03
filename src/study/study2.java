c83364b496824b3c244794708d6546fb75c1a93e
Jaewoo Choi <imagi00a@gmail.com>
2024-01-03 11:55:43 +0900

    3강_변수

---
+++ src/study/study2.java
@@ -0,0 +1,23 @@
+package study;
+
+public class study2 {
+
+	public static void main(String[] args) {
+
+		int i = 10;
+		int j;
+		j = 100;
+
+		i = 200;
+
+		System.out.println("i = " + i);
+
+		i = 300;
+
+		System.out.println("i = " + i);
+
+		System.out.println("i + j = " + (i + j));
+
+	}
+
+}
